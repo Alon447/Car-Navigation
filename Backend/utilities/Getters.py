0b422b15b0abf86b8b31d5b62c968ccb435a69d2
Alon447 <alon447@gmail.com>
2024-11-03 22:59:13 +0200

    basic changes

--- Backend/utilities/Getters.py
+++ Backend/utilities/Getters.py
@@ -132,7 +132,7 @@ def get_graph(graph_name: str):
         graph = ox.graph_from_address(graph_name, network_type='drive', dist = GRAPH_DISTANCE, simplify = True)
         modified_graph = Speeds.add_max_speed_to_graph(graph) # add max speed to the graph
         ox.save_graphml(modified_graph, filepath=path)
-
+    print("graph loaded")
     return ox.load_graphml(path),path
 
 def get_specific_directory(dir_name):
